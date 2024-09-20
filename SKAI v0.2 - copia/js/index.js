d47728326945ef207424c640e62aa69ffe34795a
projects81 <techfairy11@gmail.com>
2024-09-19 23:51:30 -0600

    Delete SKAI v0.2 - copia directory

--- SKAI v0.2 - copia/js/index.js
+++
@@ -1,5 +0,0 @@
-// Agrega un evento de clic al documento (toda la página)
-document.addEventListener("click", function() {
-    // Redirige a la página "IA.html" cuando se hace clic en cualquier lugar
-    window.location.href = "IA.html";
-});
--- SKAI v0.2 - copia/js/recarga.js
+++
@@ -1,4 +0,0 @@
-// Función para redirigir a la página deseada después de 6 segundos
-setTimeout(function() {
-    window.location.href = "index.html"; // Reemplaza "index.html" con la URL de destino
-  }, 6000); // 6000 milisegundos (6 segundos)
