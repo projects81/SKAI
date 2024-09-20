eeda35ada05ff09d84bde13c41d52ba2bd6c56c8
projects81 <techfairy11@gmail.com>
2024-09-19 22:56:51 -0600

    Add files via upload

---
+++ SKAI v0.2 - copia/js/Trashbot.js
@@ -0,0 +1,126 @@
+// Definición del modelo de reconocimiento de imágenes
+
+        // URL del modelo y los metadatos proporcionados por Teachable Machine
+        const imageURL = "https://teachablemachine.withgoogle.com/models/wLnBIqF8F/";
+        let imageModel, webcam, imageLabelContainer, maxImagePredictions;
+
+        // Función para inicializar el reconocimiento de imágenes
+        async function initImageRecognition() {
+            // Carga del modelo y los metadatos
+            const modelURL = imageURL + "model.json";
+            const metadataURL = imageURL + "metadata.json";
+
+            // Carga del modelo y obtención del número máximo de clases
+            imageModel = await tmImage.load(modelURL, metadataURL);
+            maxImagePredictions = imageModel.getTotalClasses();
+
+            // Configuración de la cámara web y el lienzo para mostrar la imagen
+            const flip = true;
+            webcam = new tmImage.Webcam(400, 400, flip);    //Se crea una instancía de lac clase Webcam
+            await webcam.setup();
+            await webcam.play();
+            window.requestAnimationFrame(imageLoop);
+
+            // Mostrar el lienzo de la cámara web en el contenedor
+            document.getElementById("webcam-container").appendChild(webcam.canvas);
+
+            // Creación de contenedores para las etiquetas de clasificación
+            imageLabelContainer = document.getElementById("image-label-container");
+            for (let i = 0; i < maxImagePredictions; i++) {
+                imageLabelContainer.appendChild(document.createElement("div"));
+            }
+        }
+
+        // Función para actualizar y predecir las imágenes continuamente
+        async function imageLoop() {
+            webcam.update();
+            await predictImage();
+            window.requestAnimationFrame(imageLoop);
+        }
+
+        // Función para realizar las predicciones con el modelo de imágenes
+        // Función para realizar las predicciones con el modelo de imágenes
+// Variables para rastrear el estado anterior
+let previousPredictionState = [];
+
+// Función para realizar las predicciones con el modelo de imágenes
+async function predictImage() {
+    const prediction = await imageModel.predict(webcam.canvas);
+    let newPredictionState = [];
+
+    for (let i = 0; i < maxImagePredictions; i++) {
+        let className = "";
+        let sendSignal = "";
+        if (i === 0) {
+            className = "Plástico";
+            sendSignal = "A";
+        } else if (i === 1) {
+            className = "Papel y Cartón";
+            sendSignal = "B";
+        } else if (i === 2) {
+            className = "Orgánico";
+            sendSignal = "C";
+        } else if (i === 3) {
+            className = "Latas y Aluminio";
+            sendSignal = "D";
+        } else if (i === 4) {
+            className = "Indefinido";
+            sendSignal = "E";
+        } 
+
+    
+        const classPrediction =
+            className + ": " + prediction[i].probability.toFixed(2);
+        imageLabelContainer.childNodes[i].innerHTML = classPrediction;
+    
+        let printValue = prediction[i].probability > 0.60 ? sendSignal : "";
+    
+        if (previousPredictionState[i] !== printValue) {
+            if (printValue !== "") {
+                console.log(`Predicción ${className}: ${printValue}`);
+                sendCommand(printValue); // Llamar a la función sendCommand con printValue
+
+                // Si se detecta "Botellas" con probabilidad > 0.70, redirigir a "animación1.html"
+                if (printValue === "C") {
+                    window.location.href = "animacion1.html";
+                }
+                if (printValue === "B") {
+                    window.location.href = "animacion2.html";
+                }
+                if (printValue === "D") {
+                    window.location.href = "animacion3.html";
+                }
+                if (printValue === "A") {
+                    window.location.href = "animacion4.html";
+                }
+            }
+        }
+        
+    
+        newPredictionState[i] = printValue;
+    }
+    
+    
+
+    // Actualizar el estado anterior
+    previousPredictionState = newPredictionState;
+
+}
+
+// Definición de la función sendCommand que recibe un parámetro "printValue"
+async function sendCommand(printValue) {
+    // Construir la URL para la solicitud HTTP en base a printValue
+    const url = `http://192.168.0.14/${printValue === 'A' ? 'A' : printValue === 'B' ? 'B' : printValue === 'C' ? 'C' : printValue === 'D' ? 'D' : printValue === 'E' ? 'E' : 'F'}`;
+
+    
+    // Realizar una solicitud fetch a la URL construida
+    const response = await fetch(url);
+    const responseData = await response.text();
+    
+    return responseData;
+}
+
+
+
+        
+        
---
+++ SKAI v0.2 - copia/js/animacion2.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var paper = document.querySelector('.paper');
+  var textEs = document.querySelector('.text-es');
+  var textEn = document.querySelector('.text-en');
+
+  // Muestra el papel y el texto inmediatamente
+  paper.style.opacity = '1';
+  textEs.style.opacity = '1';
+  textEn.style.opacity = '1';
+});
