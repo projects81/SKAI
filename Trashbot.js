fbfe31ca1ad846a1a943075fd1de1d47103d03fa
projects81 <techfairy11@gmail.com>
2024-09-21 21:59:59 -0600

    Update Trashbot.js

--- Trashbot.js
+++ Trashbot.js
@@ -109,14 +109,21 @@ async function predictImage() {
 
 // Definición de la función sendCommand que recibe un parámetro "printValue"
 async function sendCommand(printValue) {
-    const url = `http://192.168.0.14/?command=${printValue}`;
+    // Construir la URL para la solicitud HTTP en base a printValue
+    const url = `http://192.168.0.14/${printValue === 'A' ? 'A' : printValue === 'B' ? 'B' : printValue === 'C' ? 'C' : printValue === 'D' ? 'D' : printValue === 'E' ? 'E' : 'F'}`;
+
+    
+    // Realizar una solicitud fetch a la URL construida
     const response = await fetch(url);
     const responseData = await response.text();
-    console.log("Respuesta del ESP32: ", responseData);
+    
+    return responseData;
 }
 
 
 
+        
+        
 
         
         
