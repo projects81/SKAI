ad4fbd6c15dda7513ff227d057df611c321f3ebb
projects81 <techfairy11@gmail.com>
2024-09-28 00:13:39 -0600

    detallitos

--- Trashbot.js
+++ Trashbot.js
@@ -109,12 +109,12 @@ async function predictImage() {
 
 // Definición de la función sendCommand que recibe un parámetro "printValue"
 async function sendCommand(printValue) {
-    const url = http://192.168.1.184/${printValue};  // Cambia esto por la IP de tu ESP32
+    const url = 'https://192.168.0.184/${printValue}';
 
     try {
         const response = await fetch(url, {
             method: 'GET',
-            mode: 'no-cors', 
+            mode: 'cors', 
         });
 
         if (!response.ok) {
