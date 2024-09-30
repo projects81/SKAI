5c3d9de226ce90532757cd649d7d571675ce63ba
projects81 <techfairy11@gmail.com>
2024-09-29 22:00:09 -0600

    Guarla modificando pt3

--- Trashbot.js
+++ Trashbot.js
@@ -93,7 +93,7 @@ async function predictImage() {
                 if (printValue === "A") {
                     window.location.href = "animacion4.html";
                 }
-            }, 1000);
+            }, 3000);
 
 
             }
